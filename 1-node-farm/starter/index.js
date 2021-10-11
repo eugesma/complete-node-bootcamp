c8271f9dae7465e192fd5f327d0194b1bdc8efd7
eugesma@gmail.com <eugesma@gmail.com>
2021-10-11 12:17:24 -0300

    understanding sync and async nodejs block functions

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const http = require('http');
+
+/////////////////////////////////
+// FILES
+
+
+// fs.readFile('./txt/starttttt.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR!');
+
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('.txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written :D');
+//       })
+//     });
+//   });
+// });
+
+// console.log('Will read file!!');
+
+/////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+})
+
